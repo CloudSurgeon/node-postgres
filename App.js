2804e4ecc275edc048c2949c953edb7185b45290
Adam Bowen <hsic@delphix.com>
2016-08-31 21:15:08 -0400

    removed heroku stuff

--- App.js
+++ App.js
@@ -38,7 +38,7 @@ app.get('/db/dropTable', function(req,res){
 app.set('port', process.env.PORT || 3001);
 
 app.use(express.static(__dirname + '/client')); 
-app.use(express.errorHandler());
+app.use(express.error-handler());
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
 });
